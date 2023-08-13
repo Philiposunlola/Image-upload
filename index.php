1e2644b84d6c040f7c71119dd067da9bc87d2e18
Philip <philiposun@gmail.com>
2023-08-13 01:39:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    <title>Image Uplaod</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        <title>Image Uplaod</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
         <div class="container mt-3">
-        <h2>Image Uplaod</h2>
-        <form action="/action_page.php">
-            <div class="mb-3 mt-3">
-            <label for="email">upload file:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-            </div>
-            <div class="mb-3">
-            <label for="pwd">Password:</label>
-            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
-            </div>
-            <div class="form-check mb-3">
-            <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" name="remember"> Remember me
-            </label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+            <h2>Image Uplaod</h2>
+            <form action="/action_page.php">
+                <div class="mb-3 mt-3">
+                    <label for="email">upload file:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
     </body>
 </html>
