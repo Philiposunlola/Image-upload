30be082dd620376f4a67b98f077cab1df51d2116
Philip <philiposun@gmail.com>
2023-08-22 01:14:10 +0100

    image upload

--- add.php
+++ add.php
@@ -1,28 +1,14 @@
 <?php
  
     include('includes/connect.php');
-
-    if(isset($_POST['submit'])) {
-        $filename = $_FILES['doc'] ['name'];
-        $tempname = $_FILES['doc'] ['tmp_name'];
-        move_uploaded_file($tempname, 'uploads/' .$filename);
-
-        $sql = "insert into media (doc) value ('". $filename."')";
-        $result = mysqli_query($conn, $sql);
-
-        if ($result) {
-            echo 'data inserted';
-        }else {
-            echo 'error';
-        }
-    }
+ 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Image Uplaod</title>
+        <title>All images  </title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
