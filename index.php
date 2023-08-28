bb3a211a961cb2cf11652d6422d4e1a3c23db540
Philip <philiposun@gmail.com>
2023-08-28 01:52:23 +0100

    image upload

--- index.php
+++ index.php
@@ -2,29 +2,27 @@
  
     include('includes/connect.php');
 
-    if (isset($_POST['submit'])) {
-       $filename = $_FILES['doc'] ['name'];
-       $tempname = $_FILES['doc'] ['tmp_name'];
-       move_uploaded_file($tempname, 'uploads/' . $filename);
+    if (isset($_GET['delid'])) {
+        $delid = $_GET['delid'];
 
-       
-       $sql = "insert into media (doc) values ('" . $filename ."')";
-       $result = mysqli_query($conn, $sql);
+        $sql = "delete from media where doc='" . $delid . "'";
+        $result = mysqli_query($conn, $sql);
 
-       if ($result) {
-            echo 'data inserted';
-       }else {
-            echo 'error';
-       }
+        if($result) {
+            echo 'deleted';
+            header('location:index.php');
+        }else{
+            echo "error";
+        }
     }
- 
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>All images</title>
+        <title>All Images</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -32,14 +30,31 @@
     </head>
     <body>
         <div class="container mt-3">
-            <h2>Image Uplaod</h2>
-            <form method="post" enctype="multipart/form-data">
-                <div class="mb-3 mt-3">
-                    <label for="email">Upload file:</label>
-                    <input type="file" class="form-control" name="doc">
-                </div>
-                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-            </form>
+            <h2>All Images</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                <?php
+                    $sql = "SELECT * FROM media";
+                    $result = mysqli_query($conn, $sql);
+ 
+                    while($data = mysqli_fetch_array($result))
+                 {?>
+                    <tr>
+                        <td><img src="uploads/<?php echo $data['doc']; ?>" style="height: 100px;"></td>
+                        <td><a href="edit.php?editid=<?php echo $data['doc']; ?>">Edit</a></td>
+                        <td><a href="index.php?delid=<?php echo $data['doc']; ?>">Delete</a></td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>  
         </div>
     </body>
 </html>
