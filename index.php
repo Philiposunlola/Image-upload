52aff60e9071087a21b89677c66bdc6281d6de7a
Philip <philiposun@gmail.com>
2023-09-05 01:09:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -49,8 +49,8 @@
                  {?>
                     <tr>
                         <td><img src="uploads/<?php echo $data['doc']; ?>" style="height: 100px;"></td>
-                        <td><a href="edit.php?editid=<?php echo $data['doc']; ?>">Edit</a></td>
-                        <td><a href="index.php?delid=<?php echo $data['doc']; ?>">Delete</a></td>
+                        <td><a href="edit.php?editdoc=<?php echo $data['doc']; ?>">Edit</a></td>
+                        <td><a href="index.php?deldoc=<?php echo $data['doc']; ?>">Delete</a></td>
                     </tr>
                 <?php } ?>
                 </tbody>
