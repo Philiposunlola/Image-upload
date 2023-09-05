8a734e54dcf1209d7a6f82aacf4a551f22168285
Philip <philiposun@gmail.com>
2023-09-05 01:02:53 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -37,10 +37,10 @@
 ?>
 
 
-<!DOCTYPE>
+<!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Edit Image Upload</title>
+        <title></title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
